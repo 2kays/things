f6dc36a5c9c2874b38f6eee31cf4ff6d93918a03
2kays <2kays@users.noreply.github.com>
2016-10-09 01:48:40 +0100

    Added if-let

--- scratchpad.lisp
+++ scratchpad.lisp
@@ -1,13 +1,23 @@
 
 ;; Generic file for generic things/exercises etc.
 
-(defun filter-div (n list)
-  "Filters non-divisors of `n` in `list`."
-  (remove-if-not (lambda (x) (zerop (mod n x))) list))
-
 (defun divisiblep (n divisors &key (function #'mod))
   "Checks if `n` is divisible (using `function`) by all in `divisors`."
   (if (null divisors)
       t
       (and (zerop (funcall function n (car divisors)))
            (divisiblep n (cdr divisors)))))
+
+(defun divisors-only (n list)
+  "Filters non-divisors of `n` in `list`."
+  (remove-if-not (lambda (x) (divisiblep n x)) list))
+
+(defun concat (&rest strs)
+  "Alias for `concatenate 'string`."
+  (apply #'concatenate 'string strs))
+
+(defmacro if-let (bindings then-form &rest else-form)
+  `(let ,bindings
+     (if (and ,@(loop :for form :in bindings :collect (car form)))
+         ,then-form
+         ,@(when else-form else-form))))
