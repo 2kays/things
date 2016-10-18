aa80051d56a4d753e72cc6615e16b1514b895293
2kays <2kays@users.noreply.github.com>
2016-10-18 23:28:10 +0100

    Reduced nesting

--- editor.lisp
+++ editor.lisp
@@ -23,6 +23,10 @@
 (defparameter *current-buffer* nil)
 (defun current-buffer () *current-buffer*)
 
+(defmacro with-current-buffer (buffer &body body)
+  `(let ((,(first buffer) *current-buffer*))
+     ,@body))
+
 (defun concat (&rest args)
   (apply #'concatenate 'string args))
 
@@ -69,70 +73,74 @@
     (sm-helper seq 0 positions)))
 
 (defparameter *key-map*
-  '((#\So . (lambda (&rest a) (incf (buf-cursor-y *current-buffer*))))))
+  '((#\So . (lambda (&rest a)
+              (with-current-buffer (b)
+                (incf (buf-cursor-y b)))))))
 
 (defun main (&rest argv)
   "Entrypoint for the editor. ARGV should contain a file path."
-  (if (not argv)
-      (format t "error: no file specified~%")
-      (charms:with-curses ()
-        (setf *current-buffer* (make-buffer :name (first argv)
-                                            :state (file-to-list (first argv))))
-        (charms:clear-window charms:*standard-window* :force-repaint t)
-        (charms:disable-echoing)
-        (charms:enable-raw-input :interpret-control-characters t)
-        (charms:enable-non-blocking-mode charms:*standard-window*)
-        (loop :named driver-loop
-           :for c := (charms:get-char charms:*standard-window* :ignore-error t)
-           :for file-state := (buf-state *current-buffer*)
-           :for x := (buf-cursor-x *current-buffer*)
-           :for y := (buf-cursor-y *current-buffer*)
-           :do
-           (charms:refresh-window charms:*standard-window*)
-           (case c
-             ((nil) nil)
-             ;; C-fbpn movement
-             ((#\So) (incf (buf-cursor-y *current-buffer*)))
-             ((#\Dle) (decf (buf-cursor-y *current-buffer*)))
-             ((#\Ack) (incf (buf-cursor-x *current-buffer*)))
-             ((#\Stx) (decf (buf-cursor-x *current-buffer*)))
-             ;; Meta
-             ((#\Esc) (setf (elt file-state y) "Meta"))
-             ;; Return
-             ((#\Lf) (let* ((line (elt file-state y))
-                            (l1 (subseq file-state 0 y))
-                            (l2 (subseq file-state (1+ y)))
-                            (s1 (subseq line 0 x))
-                            (s2 (subseq line x)))
-                       (setf file-state (concatenate 'list l1 (list s1) (list s2) l2))
-                       (incf (buf-cursor-y *current-buffer*))
-                       (setf (buf-cursor-x *current-buffer*) 0)))
-             ;; Backspace
-             ((#\Del) (destructuring-bind (s1 _ s2)
-                          (split-many (elt file-state y) (1- x) x)
-                        (setf (elt file-state y) (concat s1 s2))
-                        (decf (buf-cursor-x *current-buffer*))))
-             ;; C-d / delete
-             ;; TODO: refactor this string splicing into one function
-             ((#\Eot #\Bs) (destructuring-bind (s1 _ s2)
-                               (split-many (elt file-state y) x (1+ x))
-                             (setf (elt file-state y) (concat s1 s2))))
-             ;; C-x quits
-             ((#\Can) (return-from driver-loop))
-             ;; 32 to 126 are printable characters
-             (t (if (and (> (char-code c) 31) (< (char-code c) 127))
-                    (destructuring-bind (s1 s2)
-                        (split-at (elt file-state y) x)
-                      (setf (elt file-state y) (format nil "~a~a~a" s1 c s2))
-                      (incf (buf-cursor-x *current-buffer*))))))
-           (multiple-value-bind (cx cy) (clamp (buf-cursor-x *current-buffer*)
-                                               (buf-cursor-y *current-buffer*)
-                                               file-state)
-             (setf (buf-cursor-x *current-buffer*) cx)
-             (setf (buf-cursor-y *current-buffer*) cy)
-             (charms:write-string-at-point charms:*standard-window*
-                                           (state-to-string file-state) 0 0)
-             (charms:move-cursor charms:*standard-window* cx cy))))))
+  (unless argv
+    (format t "error: no file specified~%")
+    (return))
+  (charms:with-curses ()
+    (setf *current-buffer*
+          (make-buffer :name (first argv)
+                       :state (file-to-list (first argv))))
+    (charms:clear-window charms:*standard-window* :force-repaint t)
+    (charms:disable-echoing)
+    (charms:enable-raw-input :interpret-control-characters t)
+    (charms:enable-non-blocking-mode charms:*standard-window*)
+    (loop :named driver-loop
+       :for c := (charms:get-char charms:*standard-window* :ignore-error t)
+       :for file-state := (buf-state *current-buffer*)
+       :for x := (buf-cursor-x *current-buffer*)
+       :for y := (buf-cursor-y *current-buffer*)
+       :do
+       (charms:refresh-window charms:*standard-window*)
+       (case c
+         ((nil) nil)
+         ;; C-fbpn movement
+         ((#\So) (incf (buf-cursor-y *current-buffer*)))
+         ((#\Dle) (decf (buf-cursor-y *current-buffer*)))
+         ((#\Ack) (incf (buf-cursor-x *current-buffer*)))
+         ((#\Stx) (decf (buf-cursor-x *current-buffer*)))
+         ;; Meta
+         ((#\Esc) (setf (elt file-state y) "Meta"))
+         ;; Return
+         ((#\Lf) (let* ((line (elt file-state y))
+                        (l1 (subseq file-state 0 y))
+                        (l2 (subseq file-state (1+ y)))
+                        (s1 (subseq line 0 x))
+                        (s2 (subseq line x)))
+                   (setf file-state (concatenate 'list l1 (list s1) (list s2) l2))
+                   (incf (buf-cursor-y *current-buffer*))
+                   (setf (buf-cursor-x *current-buffer*) 0)))
+         ;; Backspace
+         ((#\Del) (destructuring-bind (s1 _ s2)
+                      (split-many (elt file-state y) (1- x) x)
+                    (setf (elt file-state y) (concat s1 s2))
+                    (decf (buf-cursor-x *current-buffer*))))
+         ;; C-d / delete
+         ;; TODO: refactor this string splicing into one function
+         ((#\Eot #\Bs) (destructuring-bind (s1 _ s2)
+                           (split-many (elt file-state y) x (1+ x))
+                         (setf (elt file-state y) (concat s1 s2))))
+         ;; C-x quits
+         ((#\Can) (return-from driver-loop))
+         ;; 32 to 126 are printable characters
+         (t (if (and (> (char-code c) 31) (< (char-code c) 127))
+                (destructuring-bind (s1 s2)
+                    (split-at (elt file-state y) x)
+                  (setf (elt file-state y) (format nil "~a~a~a" s1 c s2))
+                  (incf (buf-cursor-x *current-buffer*))))))
+       (multiple-value-bind (cx cy) (clamp (buf-cursor-x *current-buffer*)
+                                           (buf-cursor-y *current-buffer*)
+                                           file-state)
+         (setf (buf-cursor-x *current-buffer*) cx)
+         (setf (buf-cursor-y *current-buffer*) cy)
+         (charms:write-string-at-point charms:*standard-window*
+                                       (state-to-string file-state) 0 0)
+         (charms:move-cursor charms:*standard-window* cx cy)))))
 ;;
 ;; Yet Another Messy Amateur Text Editor
 ;; YAMATE
